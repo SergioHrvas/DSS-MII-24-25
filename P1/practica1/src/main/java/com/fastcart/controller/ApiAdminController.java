14b025fbbec4d7df2a4573c12c037741bae1718f
Sergio <sergiohervas18@gmail.com>
2024-12-01 14:15:22 +0100

    Endpoint para eliminar producto

--- P1/practica1/src/main/java/com/fastcart/controller/ApiAdminController.java
+++ P1/practica1/src/main/java/com/fastcart/controller/ApiAdminController.java
@@ -1,6 +1,9 @@
 package com.fastcart.controller;
 
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +23,19 @@ public class ApiAdminController {
 
 	@PostMapping("save_product")
 	public Product saveProduct(@RequestBody Product product) {
-	    System.out.println("Producto recibido: " + product);
-
 	    Product savedProduct = productService.saveProduct(product);
 
-	    System.out.println("Producto guardado: " + savedProduct);
 	    
 		return savedProduct;
+	}
+	
+
+	@DeleteMapping("delete_product/{id}")
+	public boolean deleteProduct(@PathVariable Long id) {
+		boolean deleted = productService.deleteProduct(id);
+
+		return deleted;
+
 	}
 	
 	@PostMapping("test")
